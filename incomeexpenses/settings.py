ad40b16f051b1c45515c7f6c39625df1a4316393
unknown <elizabethokumu92@gmail.com>
2021-11-03 08:56:52 +0300

    Added the static folder

--- incomeexpenses/settings.py
+++ incomeexpenses/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'incomeexpenses.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,8 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR=[os.path.join(BASE_DIR,'incomeexpenses')]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
